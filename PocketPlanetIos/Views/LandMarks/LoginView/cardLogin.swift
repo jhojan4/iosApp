17fc2a25cdce7cbd0905734484c1ab5e5c324f53
Jhojan <asdrubalorozco4@gmail.com>
2025-06-03 17:00:34 -0500

    Pagina Inicio Sesion

---
+++ PocketPlanetIos/Views/LandMarks/LoginView/cardLogin.swift
@@ -0,0 +1,18 @@
+//
+//  cardLogin.swift
+//  PocketPlanetIos
+//
+//  Created by Telematica on 3/06/25.
+//
+
+import SwiftUI
+
+struct cardLogin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    cardLogin()
+}
---
+++ PocketPlanetIos/Views/LandMarks/LoginView/loginView.swift
@@ -0,0 +1,18 @@
+//
+//  loginView.swift
+//  PocketPlanetIos
+//
+//  Created by Telematica on 3/06/25.
+//
+
+import SwiftUI
+
+struct loginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loginView()
+}
