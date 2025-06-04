12b999bed0fa65c5c8cb1a3852bb3c8d15b3fd15
jhojan4 <asdrubalorozco4@gmail.com>
2025-06-04 11:51:05 -0500

    Conexion con base de datos y pantallas login, presentacion, registro completas

---
+++ PocketPlanetIos/Views/LandMarks/RegisterView/cardRegister.swift
@@ -0,0 +1,18 @@
+//
+//  cardRegister.swift
+//  PocketPlanetIos
+//
+//  Created by Telematica on 4/06/25.
+//
+
+import SwiftUI
+
+struct cardRegister: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    cardRegister()
+}
