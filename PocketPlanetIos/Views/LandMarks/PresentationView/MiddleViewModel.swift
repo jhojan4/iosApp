c3bd181b72c1cfe091bbb3bb7deb7294b89fadb1
jhojan4 <asdrubalorozco4@gmail.com>
2025-06-05 10:56:08 -0500

    Navigate completo, pantalla completa de Presentacion junto a Viewmodel

---
+++ PocketPlanetIos/Views/LandMarks/PresentationView/MiddleViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MiddleViewModel.swift
+//  PocketPlanetIos
+//
+//  Created by Telematica on 5/06/25.
+//
+
+import SwiftUI
+
+struct MiddleViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MiddleViewModel()
+}
