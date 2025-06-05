42134c3393fbc0d885652ea5587df3cbec02cea8
jhojan4 <asdrubalorozco4@gmail.com>
2025-06-05 11:28:15 -0500

    aautenticacion firestrore modelo de la base de dato usuarios

--- PocketPlanetIos/Model/firestoreModel.swift
+++ PocketPlanetIos/Model/firestoreModel.swift
@@ -4,5 +4,34 @@
 //
 //  Created by Telematica on 5/06/25.
 //
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
-import Foundation
+struct UserProfile: Identifiable, Codable {
+    @DocumentID var id: String?            // ID automático de Firestore (opcional)
+    
+    var userName: String
+    var email: String
+    var country: String
+    var phoneNumber: String
+    var password: String
+    var repeatPassword: String
+    
+    // Opcional: inicializador si lo necesitas
+    init(
+        userName: String = "",
+        email: String = "",
+        country: String = "",
+        phoneNumber: String = "",
+        password: String = "",
+        repeatPassword: String = ""
+    ) {
+        self.userName = userName
+        self.email = email
+        self.country = country
+        self.phoneNumber = phoneNumber
+        self.password = password
+        self.repeatPassword = repeatPassword
+    }
+}
+
